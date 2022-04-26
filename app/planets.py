b29e69c02c97456f3322dd4fe3c99e532a394381
Doina Colun <doinacolun@macpro.local>
2022-04-26 11:12:37 -0700

    Wave 2

--- app/planets.py
+++ app/planets.py
@@ -36,16 +36,33 @@ def get_all_planets():
 
 @planets_bp.route("/<planet_id>", methods=["GET"])
 def get_planet(planet_id):
-    planet_id = int(planet_id)
-    
+    chosen_planet = None
+    try:
+        planet_id = int(planet_id)
+    except ValueError:
+        rsp = {"message": f"Invalid planet id {planet_id}"}
+        return jsonify(rsp), 400
+
     for planet in planets:
         if planet_id == planet.id:
-            return {
-                "id": planet.id,
-                "name": planet.name,
-                "description": planet.description,
-                "distance_from_sun": planet.distance_from_sun
-            }
+            chosen_planet = planet
+            break
+    if chosen_planet is None:
+        rsp = {"message": f"Could not find planet with id {planet_id}"}
+        return jsonify(rsp), 404      
+            
+    rsp = {
+        "id": planet.id,
+        "name": planet.name,
+        "description": planet.description,
+        "distance_from_sun": planet.distance_from_sun
+    }
+
+    return jsonify(rsp), 200
+
+        
+
+            
             
 
 
