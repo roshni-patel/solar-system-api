94a6a6435321827f4ebb41248f25b06ac5fd8c75
Roshni Patel <rpatel01@wesleyan.edu>
2022-04-29 11:55:41 -0700

    Wave 3

---
+++ app/Planet.py
@@ -0,0 +1,13 @@
+### WAVE 2 
+# class Planet:
+#     def __init__(self, id, name, description, miles_from_sun):
+#         self.id = id 
+#         self.name = name 
+#         self.description = description
+#         self.miles_from_sun = miles_from_sun
+
+# mercury = Planet(1, "Mercury",  "the smallest planet in the Solar System and the closest to the Sun", 30.325)
+# venus = Planet(2, "Venus",  "the second planet from the Sun and is named after the Roman goddess of love and beauty", 67.24)
+# earth = Planet(3, "Earth",  "the third planet from the Sun and the only astronomical object known to harbor life.", 92.96)
+
+# PLANETS = [mercury, venus, earth]
--- app/planets.py
+++ app/planets.py
@@ -1,27 +1,46 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request 
+from app.models.planets import Planet
+from app import db 
 
-class Planet:
-    def __init__(self, id, name, description, distance_from_sun):
-        self.id = id
-        self.name = name
-        self.description = description
-        self.distance_from_sun = distance_from_sun
+## WAVE 2
+# class Planet:
+#     def __init__(self, id, name, description, distance_from_sun):
+#         self.id = id
+#         self.name = name
+#         self.description = description
+#         self.distance_from_sun = distance_from_sun
 
 
 # list of planets
-planets = [
-    Planet(1, "Earth", "A planet were humans live", 92.96),
-    Planet(2, "Mercury", "Smallest planet in the solar system", 30.325),
-    Planet(3, "Venus", "It's named after Roman goddess of love and beauty", 67.24)
+# planets = [
+#     Planet(1, "Earth", "A planet were humans live", 92.96),
+#     Planet(2, "Mercury", "Smallest planet in the solar system", 30.325),
+#     Planet(3, "Venus", "It's named after Roman goddess of love and beauty", 67.24)
     
-]
+# ]
 
+### WAVE 3 
 #Blueprint
-
 planets_bp = Blueprint("planets_bp", __name__, url_prefix="/planets")
+
+@planets_bp.route("", methods=["POST"])
+def create_one_planet():
+    request_body = request.get_json()
+    new_planet = Planet(name=request_body["name"], 
+                        description=request_body["description"],
+                        miles_from_sun=request_body["miles_from_sun"])
+
+    db.session.add(new_planet)
+    db.session.commit()
+
+    return {
+        "id": new_planet.id,
+        "msg": f"Successfully created planet with id {new_planet.id}"
+    }, 201 
 
 @planets_bp.route("", methods=["GET"])
 def get_all_planets():
+    planets = Planet.query.all()
     planet_response = []
 
     for planet in planets:
@@ -29,41 +48,51 @@ def get_all_planets():
             "id": planet.id,
             "name": planet.name,
             "description": planet.description,
-            "distance_from_sun": planet.distance_from_sun
+            "miles_from_sun": planet.miles_from_sun
         })
 
     return jsonify(planet_response)
 
-@planets_bp.route("/<planet_id>", methods=["GET"])
-def get_planet(planet_id):
-    chosen_planet = None
-    try:
-        planet_id = int(planet_id)
-    except ValueError:
-        rsp = {"message": f"Invalid planet id {planet_id}"}
-        return jsonify(rsp), 400
+### WAVE 2
+# @planets_bp.route("", methods=["GET"])
+# def get_all_planets():
+#     planet_response = []
 
-    for planet in planets:
-        if planet_id == planet.id:
-            chosen_planet = planet
-            break
-    if chosen_planet is None:
-        rsp = {"message": f"Could not find planet with id {planet_id}"}
-        return jsonify(rsp), 404      
+#     for planet in planets:
+#         planet_response.append({
+#             "id": planet.id,
+#             "name": planet.name,
+#             "description": planet.description,
+#             "distance_from_sun": planet.distance_from_sun
+#         })
+
+#     return jsonify(planet_response)
+
+# @planets_bp.route("/<planet_id>", methods=["GET"])
+# def get_planet(planet_id):
+#     chosen_planet = None
+#     try:
+#         planet_id = int(planet_id)
+#     except ValueError:
+#         rsp = {"message": f"Invalid planet id {planet_id}"}
+#         return jsonify(rsp), 400
+
+#     for planet in planets:
+#         if planet_id == planet.id:
+#             chosen_planet = planet
+#             break
+#     if chosen_planet is None:
+#         rsp = {"message": f"Could not find planet with id {planet_id}"}
+#         return jsonify(rsp), 404      
             
-    rsp = {
-        "id": planet.id,
-        "name": planet.name,
-        "description": planet.description,
-        "distance_from_sun": planet.distance_from_sun
-    }
+#     rsp = {
+#         "id": planet.id,
+#         "name": planet.name,
+#         "description": planet.description,
+#         "distance_from_sun": planet.distance_from_sun
+#     }
 
-    return jsonify(rsp), 200
-
-        
-
-            
-            
+#     return jsonify(rsp), 200
 
 
 
